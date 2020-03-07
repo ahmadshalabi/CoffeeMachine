6765388b869471a6c06ac9be77ec1aceca3dc051
ahmadshalabi <eng.ahmadshalabi@gmail.com>
2020-03-08 01:02:51 +0200

    Stage #3: Enough coffee for everyone

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -3,18 +3,39 @@ package machine;
 import java.util.Scanner;
 
 public class CoffeeMachine {
+    final static Scanner reader = new Scanner(System.in);
     private static final int WATER_OF_ONE_CUP_OF_COFFEE = 200;
     private static final int MILK_OF_ONE_CUP_OF_COFFEE = 50;
     private static final int BEANS_OF_ONE_CUP_OF_COFFEE = 15;
 
     public static void main(String[] args) {
-        final Scanner reader = new Scanner(System.in);
+        int water = read("Write how many ml of water the coffee machine has:");
+        int milk = read("Write how many ml of milk the coffee machine has:");
+        int coffeeBeans = read("Write how many grams of coffee beans the coffee machine has:");
+        int cupsOfCoffee = read("Write how many cups of coffee you will need:");
 
-        System.out.println("Write how many cups of coffee you will need:");
-        int cupsOfCoffee = reader.nextInt();
-        System.out.printf("For %d cups of coffee you will need:%n", cupsOfCoffee);
-        System.out.printf("%d ml of water%n", cupsOfCoffee * WATER_OF_ONE_CUP_OF_COFFEE);
-        System.out.printf("%d ml of milk%n", cupsOfCoffee * MILK_OF_ONE_CUP_OF_COFFEE);
-        System.out.printf("%d g of coffee beans%n", cupsOfCoffee * BEANS_OF_ONE_CUP_OF_COFFEE);
+        int possibleCupsOfCoffee = calculatePossibleCupsOfCoffee(water, milk, coffeeBeans);
+        coffeeMachineResponse(cupsOfCoffee, possibleCupsOfCoffee);
+    }
+
+    private static void coffeeMachineResponse(int cupsOfCoffee, int possibleOfCupCoffee) {
+        if (possibleOfCupCoffee == cupsOfCoffee) {
+            System.out.println("Yes, I can make that amount of coffee");
+        } else if (possibleOfCupCoffee < cupsOfCoffee) {
+            System.out.printf("No, I can make only %d cup(s) of coffee%n", possibleOfCupCoffee);
+        } else {
+            System.out.printf("Yes, I can make that amount of coffee (and even %d more than that)", possibleOfCupCoffee - cupsOfCoffee);
+        }
+    }
+
+    private static int calculatePossibleCupsOfCoffee(int water, int milk, int coffeeBeans) {
+        int possibleCupsOfCoffee = Math.min(water / WATER_OF_ONE_CUP_OF_COFFEE, milk / MILK_OF_ONE_CUP_OF_COFFEE);
+        possibleCupsOfCoffee = Math.min(possibleCupsOfCoffee, coffeeBeans / BEANS_OF_ONE_CUP_OF_COFFEE);
+        return possibleCupsOfCoffee;
+    }
+
+    private static int read(String s) {
+        System.out.println(s);
+        return reader.nextInt();
     }
 }
