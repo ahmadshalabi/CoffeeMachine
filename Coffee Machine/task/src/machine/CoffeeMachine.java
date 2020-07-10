936503141c27e527b282a1fb62ae771f6dcfaeb9
ahmadshalabi <eng.ahmadshalabi@gmail.com>
2020-07-10 05:06:12 +0300

    CCU: Rewrite CoffeeMachine

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -1,41 +1,95 @@
 package machine;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
 
 public class CoffeeMachine {
-    final static Scanner reader = new Scanner(System.in);
+    private static final Scanner reader = new Scanner(System.in);
     private static final int WATER_OF_ONE_CUP_OF_COFFEE = 200;
     private static final int MILK_OF_ONE_CUP_OF_COFFEE = 50;
     private static final int BEANS_OF_ONE_CUP_OF_COFFEE = 15;
 
     public static void main(String[] args) {
-        int water = read("Write how many ml of water the coffee machine has:");
-        int milk = read("Write how many ml of milk the coffee machine has:");
-        int coffeeBeans = read("Write how many grams of coffee beans the coffee machine has:");
-        int cupsOfCoffee = read("Write how many cups of coffee you will need:");
+        int water = prompt("Write how many ml of water the coffee machine has:");
+        int milk = prompt("Write how many ml of milk the coffee machine has:");
+        int coffeeBeans = prompt("Write how many grams of coffee beans the coffee machine has:");
+        int neededCoffeeCups = prompt("Write how many cups of coffee you will need:");
 
-        int possibleCupsOfCoffee = calculatePossibleCupsOfCoffee(water, milk, coffeeBeans);
-        coffeeMachineResponse(cupsOfCoffee, possibleCupsOfCoffee);
+        int waterBasedCoffeeCups = getWaterBasedCoffeeCups(water);
+        int milkBasedCoffeeCups = getMilkBasedCoffeeCups(milk);
+        int coffeeBeansBasedCoffeeCups = getCoffeeBeansBasedCoffeeCups(coffeeBeans);
+        int availableCoffeeCups = getAvailableCoffeeCups(waterBasedCoffeeCups, milkBasedCoffeeCups, coffeeBeansBasedCoffeeCups);
+        String response = getResponse(neededCoffeeCups, availableCoffeeCups);
+        System.out.println(response);
     }
 
-    private static void coffeeMachineResponse(int cupsOfCoffee, int possibleOfCupCoffee) {
-        if (possibleOfCupCoffee == cupsOfCoffee) {
-            System.out.println("Yes, I can make that amount of coffee");
-        } else if (possibleOfCupCoffee < cupsOfCoffee) {
-            System.out.printf("No, I can make only %d cup(s) of coffee%n", possibleOfCupCoffee);
+    /**
+     *
+     * @param neededCoffeeCups user needs
+     * @param availableCoffeeCups CoffeeCups that the machine can produce
+     * @return response based on needed and available coffee cups
+     */
+
+    private static String getResponse(int neededCoffeeCups, int availableCoffeeCups) {
+        String response;
+        if (availableCoffeeCups == neededCoffeeCups) {
+            response = "Yes, I can make that amount of coffee";
+        } else if (availableCoffeeCups < neededCoffeeCups) {
+            response = String.format("No, I can make only %d cup(s) of coffee%n", availableCoffeeCups);
         } else {
-            System.out.printf("Yes, I can make that amount of coffee (and even %d more than that)", possibleOfCupCoffee - cupsOfCoffee);
+            response = String.format("Yes, I can make that amount of coffee (and even %d more than that)", availableCoffeeCups - neededCoffeeCups);
         }
+        return response;
     }
 
-    private static int calculatePossibleCupsOfCoffee(int water, int milk, int coffeeBeans) {
-        int possibleCupsOfCoffee = Math.min(water / WATER_OF_ONE_CUP_OF_COFFEE, milk / MILK_OF_ONE_CUP_OF_COFFEE);
-        possibleCupsOfCoffee = Math.min(possibleCupsOfCoffee, coffeeBeans / BEANS_OF_ONE_CUP_OF_COFFEE);
-        return possibleCupsOfCoffee;
+    /**
+     *
+     * @param waterBasedCoffeeCups CoffeeCups the machine can produce based on available water
+     * @param milkBasedCoffeeCups CoffeeCups the machine can produce based on available milk
+     * @param coffeeBeansBasedCoffeeCups CoffeeCups the machine can produce based on available coffee beans
+     * @return available coffee cups
+     */
+
+    private static int getAvailableCoffeeCups(int waterBasedCoffeeCups, int milkBasedCoffeeCups, int coffeeBeansBasedCoffeeCups) {
+        List<Integer> coffeeCups = new ArrayList<>(3);
+        coffeeCups.add(waterBasedCoffeeCups);
+        coffeeCups.add(milkBasedCoffeeCups);
+        coffeeCups.add(coffeeBeansBasedCoffeeCups);
+        return coffeeCups.stream().min(Comparator.naturalOrder()).orElse(0);
     }
 
-    private static int read(String s) {
-        System.out.println(s);
+    /**
+     * @param water water capacity in ml
+     * @return coffee cups that can be made form available water
+     */
+    private static int getWaterBasedCoffeeCups(int water) {
+        return water / WATER_OF_ONE_CUP_OF_COFFEE;
+    }
+
+    /**
+     * @param milk milk capacity in ml
+     * @return coffee cups that can be made form available milk
+     */
+    private static int getMilkBasedCoffeeCups(int milk) {
+        return milk / MILK_OF_ONE_CUP_OF_COFFEE;
+    }
+
+    /**
+     * @param coffeeBeans coffee beans capacity in g
+     * @return coffee cups that can be made form available coffee beans
+     */
+    private static int getCoffeeBeansBasedCoffeeCups(int coffeeBeans) {
+        return coffeeBeans / BEANS_OF_ONE_CUP_OF_COFFEE;
+    }
+
+    /**
+     * @param msg message explain what the user MUST be enter as input
+     * @return see {@link Scanner#nextInt()} return
+     */
+    private static int prompt(String msg) {
+        System.out.println(msg);
         return reader.nextInt();
     }
 }
